535483b519078ef997854503ac1921dce149d272
rivermh <kmh000904@gmail.com>
2025-10-11 22:53:31 +0900

    20251011

--- spring-login-demo/src/main/java/com/example/controller/MemberController.java
+++ spring-login-demo/src/main/java/com/example/controller/MemberController.java
@@ -1,23 +1,44 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.entity.Member;
 import com.example.service.MemberService;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
-	
+
 	private final MemberService memberService;
-	
-	// 회원가입 폼
-	@GetMapping("/register")
-	public String registerForm() {
-		return "register"; // templates/register.html
+
+	// 회원가입 폼 페이지 이동
+	@GetMapping("/signup")
+	public String signupForm() {
+		return "signup"; // templates/signup.html
+	}
+
+	// 회원가입 처리
+//	@PostMapping("/signup")
+	//public String signup(@RequestParam String userId, @RequestParam String username, @RequestParam String password,
+	//		HttpSession session, Model model) {
+
+	/*	Member member = memberService.login(userId, password);
+
+		if (member != null) {
+			session.setAttribute("loginUser", member); // 세션 저장
+			return "redirect:/"; // 홈 또는 마이페이지
+		} else {
+			model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
+			return "login";
+		} */
 	}
 }
--- spring-login-demo/src/main/java/com/example/service/MemberService.java
+++ spring-login-demo/src/main/java/com/example/service/MemberService.java
@@ -15,14 +15,14 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
  
-    // 회원 조회 (userId 기준)
-    public Member findByUserId(String userId) {
-        return memberRepository.findByUserId(userId).orElse(null);
-    }
-
-    // 회원 저장
-    @Transactional
-    public Member register(Member member) {
-        return memberRepository.save(member);
+   // 회원가입 (중복 체크 포함)
+    @Transactional // 회원 저장은 readOnly = false
+    public Member signup(Member member) {
+    	Member existingMember = memberRepository.findByUserId(member.getUserId()).orElse(null);
+    	
+    	if(existingMember != null) {
+    		throw new IllegalStateException("이미 존재하는 아이디입니다.");
+    	}
+    	return memberRepository.save(member);
     }
 }
