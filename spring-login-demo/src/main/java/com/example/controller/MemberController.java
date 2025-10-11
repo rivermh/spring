be30f4ac43888b352cc1cc8e954d3c978d6671ab
rivermh <kmh000904@gmail.com>
2025-10-11 22:25:49 +0900

    20251011

---
+++ spring-login-demo/src/main/java/com/example/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+}
