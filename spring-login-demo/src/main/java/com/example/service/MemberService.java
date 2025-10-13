637192d9ae9c5db5f3e69974e08dbc59f44629da
rivermh <kmh000904@gmail.com>
2025-10-13 16:54:48 +0900

    20251013

--- spring-login-demo/src/main/java/com/example/service/MemberService.java
+++ spring-login-demo/src/main/java/com/example/service/MemberService.java
@@ -13,16 +13,17 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
+	
+	// 회원가입 (중복 체크 포함)
+	@Transactional // 회원 저장은 readOnly = false
+	public Member signUp(Member member) {
+		Member existingMember = memberRepository.findByUserId(member.getUserId()).orElse(null);
+		
+		if(existingMember != null) {
+			throw new IllegalStateException("이미 존재하는 아이디입니다.");
+		}
+		return memberRepository.save(member);
+	}
  
-   // 회원가입 (중복 체크 포함)
-    @Transactional // 회원 저장은 readOnly = false
-    public Member signup(Member member) {
-    	Member existingMember = memberRepository.findByUserId(member.getUserId()).orElse(null);
-    	
-    	if(existingMember != null) {
-    		throw new IllegalStateException("이미 존재하는 아이디입니다.");
-    	}
-    	return memberRepository.save(member);
-    }
 }
