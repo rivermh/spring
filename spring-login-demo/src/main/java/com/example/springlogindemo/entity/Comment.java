3d6f3031f68043772ae819d4f483a8d3b39b04ad
rivermh <kmh000904@gmail.com>
2025-10-21 17:47:52 +0900

    20251021

--- spring-login-demo/src/main/java/com/example/springlogindemo/entity/Comment.java
+++ spring-login-demo/src/main/java/com/example/springlogindemo/entity/Comment.java
@@ -1,5 +1,41 @@
 package com.example.springlogindemo.entity;
 
-public class Comment {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 500)
+	private String content;
+	
+	//댓글 작성자
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private Member member;
+	
+	// 댓글이 달린 게시글
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
